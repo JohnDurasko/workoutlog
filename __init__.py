665eac87af871822af8716cf4b34df89b83b3b88
Aragorn <rideforruin777@gmail.com>
2022-10-19 18:03:08 -0700

    initial commit

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'cc40a4d1ef0c90a3aac101215b5eeea6'
+app.config['SECRET_KEY'] = 'XXXXXXXXXXXXXXX'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
